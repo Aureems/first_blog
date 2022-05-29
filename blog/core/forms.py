e907203da17a820b1af92878197cbf394d06314c
Aureems <95098099+Aureems@users.noreply.github.com>
2022-05-29 13:56:03 +0300

    2commit

--- blog/core/forms.py
+++ blog/core/forms.py
@@ -5,6 +5,11 @@ from django.contrib.auth.models import User
 
 
 class RegisterForm(UserCreationForm):
+    username = forms.CharField(help_text=False)
+    email = forms.CharField(help_text=False)
+    password1 = forms.CharField(widget=forms.PasswordInput(), help_text=False, label='Password')
+    password2 = forms.CharField(widget=forms.PasswordInput(), help_text=False, label='Confirm Password')
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
--- blog/core/urls.py
+++ blog/core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, register, login_user, log_out, profile
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('register/', register, name="register"),
+    path('login/', login_user, name="login_user"),
+    path('logout/', log_out, name='logout'),
+    path('profile/', profile, name='profile')
 ]
